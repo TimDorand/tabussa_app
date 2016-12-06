018271d05f5e20708d576f531bc2f5d2fa26c19e
timothee <tim.dorand@gmail.com>
2016-12-06 18:27:31 +0100

    trying to get post data back

--- client/main.js
+++ client/main.js
@@ -41,7 +41,10 @@ $( function() {
 
 // Calling API for all drinks
 
-var drinks = [];
+var drinks = []; // tableau des brevages uniquement ['vodka', 'rhum']
+var allDrinks = []; // tableau des brevages entiers [{'id':'81', 'name':'vodka', 'color':'blue', 'type':'alcool'}, {}, {}, ... ]
+
+
 
 HTTP.call( 'POST', 'http://timothee-dorand.fr/tabussa/API/drinks', {
 
@@ -53,8 +56,10 @@ HTTP.call( 'POST', 'http://timothee-dorand.fr/tabussa/API/drinks', {
         arrayDrinks = JSON.parse(response.content);
         for(i=0; i < arrayDrinks.length; i++){
             drinks.push(arrayDrinks[i].name);
+            allDrinks.push(arrayDrinks[i]);
         }
         console.log(drinks);
+        console.log(allDrinks);
 
     }
 });
@@ -73,20 +78,88 @@ $( function() { // input de recherche des tags
 
 Ingredients = new Mongo.Collection('ingredients');
 
-Ingredients._collection.insert({ name: "Vokda", score: 0 });
-Ingredients._collection.insert({ name: "Pastis", score: 10 });
+// Ingredients._collection.insert({ name: "Vokda" });
+// Ingredients._collection.insert({ name: "Pastis" });
 
 Template.ingredients.helpers({
     'ingredients': function(){
         return Ingredients.find();
     }});
 
+var mycocktail = [];
+
 Template.addIngredients.events({
     'submit form': function (event) {
         event.preventDefault();
         var ingredientName = $('[name="ingredientName"]').val();
+
+        // A chaque submit on ajoute le nom de l'ingrédient dans un tableau
+        for(i=0; i < allDrinks.length; i++) {
+            if (ingredientName == allDrinks[i].name) {
+                var ingredientId = allDrinks[i].id;
+                var flagingredient = true;
+            }
+        }
+        if(flagingredient == true){
+            mycocktail.push(ingredientId);
+        }else{
+            console.log('pas d ingredient trouve');
+        }
         Ingredients._collection.insert({
             name: ingredientName
         });
+        console.log(mycocktail);
+        myCocktailSuggestions(mycocktail)
+
     }
 });
+
+
+// Récupérer les infos du dernier cocktail envoyé et les suggestions
+
+/* Methode
+1) Post array drinks [
+    0 => "id_cocktail_1",
+    1 => 'id_cocktail_2'
+]
+
+2) Récupérer les infos de "Mon cocktail"
+3) Récupérer toutes suggestions et les infos de chacune
+
+*/
+
+function myCocktailSuggestions(mycocktail) {
+
+    var suggestions= [];
+    // Calling a method
+    $.ajax({
+        method: "post",
+        url: "http://timothee-dorand.fr/tabussa/API/cocktail",
+        data: 'mes boissons',
+        contentType: "application/json",
+        success: function (data) {
+            console.log(data); // 6
+            console.log(data.body);
+
+        }
+    });
+/*
+    HTTP.call('POST', 'http://localhost:8888/tabussa/API/cocktail', {
+        data: {
+            "drinks": "mes boissons"
+        }
+    }, function (error, response) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(response);
+            arrayCocktails = JSON.parse(response.content);
+            for (i = 0; i < arrayCocktails.length; i++) {
+                suggestions.push(arrayCocktails[i].name);
+            }
+            console.log('reponse: ');
+            console.log(suggestions);
+
+        }
+    });*/
+}
