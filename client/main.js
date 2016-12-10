b9dcf5d11095558f8c1c59d68540384630a0f3ef
timothee <tim.dorand@gmail.com>
2016-12-10 19:41:06 +0100

    New messages, try to give +1, but API doesn't work

--- client/main.js
+++ client/main.js
@@ -6,6 +6,25 @@ import '/imports/library/jquery-ui.css';
 import '/imports/library/touch-punch';
 
 
+
+ /*-------------------------------------------------------------------*/
+// CONTENU - TABLE DES MATIERES
+/*-------------------------------------------------------------------*/
+
+/*
+* 1. Récupération des ingrédients
+* 2. Listing des ingrédients
+* 3. Ajout d'un nouvel ingrédient dans le cocktail
+* 4. Récupération des suggestions et ajout au click sur la liste
+* 5. Réinitialisation du cocktail
+* 6. Envoi d'un like
+* 7. Envoi d'un dislike
+* 8. Récupération des infos du dernier cocktail envoyé et les suggestions
+* 9. Génération du visuel du cocktial
+* 10. Initialisation du message utilisateur
+*  */
+
+
 NProgress.start();
 // Do something, like loading...
 NProgress.done();
@@ -47,13 +66,13 @@ $(document).ready(function(){
 
 
 /*-------------------------------------------------------------------*/
-// Calling API for all drinks
+// 1. Récupération de tous les ingrédients
 /*-------------------------------------------------------------------*/
 
 
 var drinks = []; // tableau des brevages uniquement ['vodka', 'rhum']
 var allDrinks = []; // tableau des brevages entiers [{'id':'81', 'name':'vodka', 'color':'blue', 'type':'alcool'}, {}, {}, ... ]
-
+var allCocktails = [];
 
 
 HTTP.call( 'POST', 'http://timothee-dorand.fr/tabussa/API/drinks', {
@@ -72,9 +91,20 @@ HTTP.call( 'POST', 'http://timothee-dorand.fr/tabussa/API/drinks', {
 
             allDrinks.push(arrayDrinks[i]);
         }
-        // console.log(drinks);
-        // console.log(allDrinks);
+    }
+});
 
+HTTP.call( 'POST', 'http://timothee-dorand.fr/tabussa/API/cocktails', {
+
+}, function( error, response ) {
+    if ( error ) {
+        console.log( error );
+    } else {
+        var arrayCocktails = JSON.parse(response.content);
+        /*{"id":"2","visits":"12","created_at":"2016-11-15 17:43:55","id_drink1":"81","id_drink2":"82","id_drink3":"84","id_drink4":"85","id_drink5":"0","id_drink6":"0","id_drink7":"0","id_drink8":"0","id_drink9":"0","id_drink10":"0","bonus":"0","malus":"0"},*/
+        for(i=0; i < arrayCocktails.length; i++){
+            allCocktails.push(arrayCocktails[i]);
+        }
     }
 });
 
@@ -89,7 +119,7 @@ $( function() { // input de recherche des tags
 
 
 /*-------------------------------------------------------------------*/
-// Listing des ingrédients du cocktail
+// 2. Listing des ingrédients du cocktail
 /*-------------------------------------------------------------------*/
 
 
@@ -104,7 +134,7 @@ Template.ingredients.helpers({
 var mycocktail = [];
 
 /*-------------------------------------------------------------------*/
-// Nouvel ingredient dans le cocktail
+// 3. Ajout d'un nouvel ingredient dans le cocktail
 /*-------------------------------------------------------------------*/
 
 
@@ -124,48 +154,31 @@ Template.addIngredients.events({
                 var ingredientColor = allDrinks[i].color;
                 var flagingredient = true;
                 console.log(mycocktail+1);
-
-
             }
         }
 
         if(flagingredient == true){
             mycocktail.push(ingredientId);
             console.log(mycocktail);
+            Ingredients._collection.insert({
+                name: ingredientName,
+                color: ingredientColor
+            });
+            new_boisson_cocktail();
+            myCocktailSuggestions(mycocktail);
+
         }else{
             console.log('pas d ingredient trouve');
+            MessageUser._collection.remove({});
+            MessageUser._collection.insert({ message: "Désolé, ça n'existe pas..." });
+
         }
-        var boisson_svg="";
-        /*switch(mycocktail.length){
-            case 1:
 
-                id=mycocktail[0];
-                console.log("id : "+mycocktail[0]);
-                boisson_svg='<path d="M0 0, L20 0, L15 25, L5 25z" fill="blue" />';
-                break;
-            case 2:
-                boisson_svg='<path d="M0 0, L20 0, L17.5 12.5, L2.5 12.5z" fill="<blue" />';
-                break;
-            default:
-                break;
-        }*/
-        new_boisson_cocktail();
-
-
-
-        Ingredients._collection.insert({
-            name: ingredientName,
-            color: ingredientColor
-        });
-
-        $('#cocktailInfoRating').html("");
-
-        myCocktailSuggestions(mycocktail);
     },
 });
 
 /*-------------------------------------------------------------------*/
-// Ajout d'une suggestion au click sur la liste
+// 4. Récupération des suggestions et ajout au click sur la liste
 /*-------------------------------------------------------------------*/
 
 
@@ -203,27 +216,9 @@ Template.suggestions.events({
         console.log(mycocktail);
         myCocktailSuggestions(mycocktail);
 
-
     }
 });
 
-/*------------------------------------------------------------*/
-// Reinitialisation de tout
-/*-------------------------------------------------------------------*/
-
-Template.ingredients.events({
-    'click #clearAll': function(event){
-        event.preventDefault();
-
-        Ingredients._collection.remove({});
-        Suggestions._collection.remove({});
-        CocktailInfo._collection.remove({});
-        MessageUser._collection.remove({});
-        MessageUser._collection.insert({ message: "Oouai ! Cul sec !" });
-
-
-    }
-});
 
 /*-------------------------------------------------------------------*/
 // Listing des suggestions
@@ -237,8 +232,117 @@ Template.suggestions.helpers({
     }});
 
 
+/*------------------------------------------------------------*/
+// 5. Reinitialisation de tout
 /*-------------------------------------------------------------------*/
-// Récupérer les infos du dernier cocktail envoyé et les suggestions
+
+Template.ingredients.events({
+    'click #clearAll': function(event){
+        event.preventDefault();
+
+        Ingredients._collection.remove({});
+        Suggestions._collection.remove({});
+        CocktailInfo._collection.remove({});
+        MessageUser._collection.remove({});
+        MessageUser._collection.insert({ message: "Oouai ! Cul sec !" });
+
+        mycocktail = [];
+
+
+    }
+});
+
+/*------------------------------------------------------------*/
+// 6. Envoi d'un like
+/*-------------------------------------------------------------------*/
+
+var id_cocktail;
+Template.cocktail.events({
+    'click .like2': function(event){
+        event.preventDefault();
+
+        /*URL : API/bonus
+         Utiliser la méthode POST en envoyant un tableau de type
+         $_POST["id"]="id_cocktail";
+
+         Il faut récupérer le tableau de tous les cocktails créé
+
+         Vous allez recevoir un tableau JSON avec les likes mis à jour*/
+
+
+
+        for(i=0; i < allCocktails.length; i++){
+            if(mycocktail[0] == allCocktails[i].id_drink1) {
+                // console.log('le premier ingrédient est bien ' + mycocktail[0]);
+                if (allCocktails[i].id_drink2 != 0) {
+                    // console.log(allCocktails[i].id + 'il y a un deuxième ingrédient');
+
+
+
+                    for(i=0; i < allCocktails.length; i++){
+                        if(mycocktail[1] == allCocktails[i].id_drink2) {
+                            // console.log('le 2ème ingrédient est bien ' + mycocktail[1]);
+                            if (allCocktails[i].id_drink3 != 0) {
+                                // console.log(allCocktails[i].id + 'il y a un 3ème ingrédient');
+
+
+
+
+
+                            }else{
+                                // console.log(allCocktails[i].id + 'il n\'y a pas de troisième ingrédient');
+                                id_cocktail = allCocktails[i].id;
+                            }
+                        }
+                    }
+
+                    } else {
+                    // console.log(allCocktails[i].id + 'il n\'y a pas de deuxième ingrédient');
+                    id_cocktail = allCocktails[i].id;
+                }
+            }
+
+        }
+
+        console.log(id_cocktail);
+
+
+
+
+
+            HTTP.call( 'POST', 'http://timothee-dorand.fr/tabussa/API/bonus', {
+            data: {
+                "id": id_cocktail
+            }
+
+        }, function( error, response ) {
+            if ( error ) {
+                console.log( error );
+            } else {
+                console.log(response);
+                MessageUser._collection.remove({});
+                MessageUser._collection.insert({ message: "C'est vrai que c'est bon... +1 !" });
+
+                /*
+                arrayDrinks = JSON.parse(response.content);
+                for(i=0; i < arrayDrinks.length; i++){
+                    drinks.push(arrayDrinks[i].name);
+                    if(arrayDrinks[i].color){
+                        drinks.push(arrayDrinks[i].color);
+                    }
+
+                    allDrinks.push(arrayDrinks[i]);
+                }*/
+                // console.log(drinks);
+                // console.log(allDrinks);
+
+            }
+        });
+    }
+});
+
+/*-------------------------------------------------------------------*/
+// 8. Récupération des infos du dernier cocktail envoyé et les suggestions
 /*-------------------------------------------------------------------*/
 
 /* Methode
@@ -382,7 +486,7 @@ function myCocktailSuggestions(mycocktail) {
 
 
 /*-------------------------------------------------------------------*/
-// Génération du cocktail visuel
+// 9. Génération du cocktail visuel
 /*-------------------------------------------------------------------*/
 
 function new_boisson_cocktail(){
